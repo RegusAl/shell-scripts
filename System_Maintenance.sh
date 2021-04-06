7156a1c040aa52561ae27410db509ab139b57ad0
RegusAl <regusal@pm.me>
2021-04-06 11:50:31 +0300

    update

--- System_Maintenance.sh
+++ System_Maintenance.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 echo -e "\n$(date "+%d-%m-%Y --- %T") --- Starting Work...\n"
 
+#update && upgrade
 apt-get update
 apt-get -y upgrade
 
+#autoremove && autoclean
 apt-get -y autoremove
 apt-get autoclean
 
